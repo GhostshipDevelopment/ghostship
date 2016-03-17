fea517c04d606a215e2dbe878882611e0afd903e
ThatITNinja <ThatITNinja@hotmail.com>
2016-03-17 00:37:23 -0400

    Bunch of work on view for nodes

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,8 @@ class Service < ActiveRecord::Base
   belongs_to :node
   has_many :credentials
   accepts_nested_attributes_for :credentials
+  
+  def full_url
+    return "#{self.node.address + self.path}"
+  end
 end
